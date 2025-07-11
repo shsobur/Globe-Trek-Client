102591e6747417d287c4f7ed7aa5790829767780
shsobur <shsoburhossen951@gmail.com>
2025-07-11 12:44:59 +0600

    Profile page updated

--- src/Layouts/Pages/Global/Profile/Profile.jsx
+++ src/Layouts/Pages/Global/Profile/Profile.jsx
@@ -1,6 +1,9 @@
+import useUserData from "../../../Hooks/useUserData";
 import "./Profile.css";
 
 const Profile = () => {
+  const { currentUserData } = useUserData();
+
   return (
     <>
       <section id="profile_section">
@@ -8,27 +11,25 @@ const Profile = () => {
           <div className="profile_content_parent_container">
             <div className="profile_content_row_one">
               <div className="profile_image">
-                <img
-                  src="https://img.freepik.com/free-photo/beautiful-girl-stands-park_8353-5084.jpg"
-                  alt="Profile Image"
-                />
+                <img src={currentUserData?.userPhoto} alt="Profile Image" />
               </div>
               <div className="profile_name_and_role">
                 <ul className="profile_basic_info_one">
-                  <li>SOBUR HOSSEN</li>
-                  <li>Tour Guide</li>
+                  <li>{currentUserData?.userName}</li>
+                  <li>{currentUserData?.userRole}</li>
                 </ul>
 
-                <ul className="profile_basic_info_two">
-                  <li className="profile_verify_style">Status: Not-verified</li>
-                  {/* <li className="profile_not_verify_style">Status: Verified</li> */}
-                </ul>
+                {currentUserData?.userRole !== "Admin" && (
+                  <ul className="profile_basic_info_two">
+                    <li className="profile_verify_style">
+                      Status: Not-verified
+                    </li>
+                    {/* <li className="profile_not_verify_style">Status: Verified</li> */}
+                  </ul>
+                )}
 
                 <ul className="profile_basic_info_three">
-                  <h1>
-                    Uniquely conceptualize go forward products through strategic
-                    web. Competently facilitate just in time systems for fully.
-                  </h1>
+                  <h1>{currentUserData?.userBio}</h1>
                 </ul>
               </div>
             </div>
@@ -39,25 +40,22 @@ const Profile = () => {
             <div className="profile_content_row_two">
               <ul>
                 <li>
-                  Email: <span>shsoburhossen951@gmail.com</span>
+                  Email: <span>{currentUserData?.userContactEmail}</span>
                 </li>
                 <li>
-                  Address: <span>Bhola, Barishal, Bangladesh</span>
+                  Address: <span>{currentUserData?.userAddress}</span>
                 </li>
                 <li>
-                  Phone: <span>+880 1787592274</span>
+                  Phone: <span>{currentUserData?.userContact}</span>
                 </li>
                 <li>
-                  Gender: <span>Male</span>
+                  NID Number: <span>{currentUserData?.userNidNumber}</span>
                 </li>
                 <li>
-                  NID Number: <span>20357089267</span>
+                  Language: <span>{currentUserData?.languages}</span>
                 </li>
                 <li>
-                  Language: <span>Bangla, Hind, English</span>
-                </li>
-                <li>
-                  Available Status: <span>Sun, Mun, Fri</span>
+                  Available Status: <span>{currentUserData?.available}</span>
                 </li>
               </ul>
             </div>
@@ -65,36 +63,40 @@ const Profile = () => {
             <hr />
             <hr />
 
-            <div className="profile_content_row_three">
-              <h1>
-                <u>
-                  <b>
-                    <i>Experience:</i>
-                  </b>
-                </u>
-              </h1>
-              <article>
-                Interactively simplify multidisciplinary expertise with
-                e-business experiences. Energistically pursue high-quality
-                mindshare rather than cross-platform relationships. Competently
-                transition tactical supply chains and ubiquitous.
-              </article>
-            </div>
+            {currentUserData?.userRole !== "Admin" && (
+              <div className="profile_content_row_three">
+                <h1>
+                  <u>
+                    <b>
+                      <i>Experience:</i>
+                    </b>
+                  </u>
+                </h1>
+                <article>
+                  Interactively simplify multidisciplinary expertise with
+                  e-business experiences. Energistically pursue high-quality
+                  mindshare rather than cross-platform relationships.
+                  Competently transition tactical supply chains and ubiquitous.
+                </article>
+              </div>
+            )}
 
             <hr />
             <hr />
 
-            <div className="profile_content_row_four">
-              <div className="profile_guide_rating_info">
-                <h3>Rating</h3>
-                <p>4.2 ★</p>
-              </div>
+            {currentUserData?.userRole !== "Admin" && (
+              <div className="profile_content_row_four">
+                <div className="profile_guide_rating_info">
+                  <h3>Rating</h3>
+                  <p>4.2 ★</p>
+                </div>
 
-              <div className="profile_guide_rating_info">
-                <h3>Booking Count</h3>
-                <p>03 🕒</p>
+                <div className="profile_guide_rating_info">
+                  <h3>Booking Count</h3>
+                  <p>03 🕒</p>
+                </div>
               </div>
-            </div>
+            )}
 
             <div className="profile_content_row_five">
               <h1>Join at: 22-06-2024</h1>
